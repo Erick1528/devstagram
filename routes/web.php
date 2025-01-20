079677b40c92989ff8d9cc60680ecc5f095a71c0
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-01-20 17:08:33 +0100

    Eliminar rutas de controladores de Comentario, Seguidor y Me gusta

--- routes/web.php
+++ routes/web.php
@@ -31,15 +31,6 @@ Route::get('/posts/create', [PostController::class, 'create'])->name('posts.crea
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
-
-// Likes
-Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
-Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
-
-// Follow
-Route::post('/{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
-Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');
